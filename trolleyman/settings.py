94cde8ee5af9914053939f5d7859aeae707f70be
Callum Tolley <cgtrolley@gmail.com>
2017-04-08 02:31:06 +0100

    Added FlappyClone app

--- trolleyman/settings.py
+++ trolleyman/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&-7+nu*f&dsf7tr8dgeg&l&pbxckmy75qt1!n9z=eiwlnhxdcx'
+with open('SECRET_KEY', 'r') as f:
+    SECRET_KEY = f.read()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
