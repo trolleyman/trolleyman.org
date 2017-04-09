9d2e46e358003f3a9b88ac8d17eb7277aec8bca2
Callum Tolley <cgtrolley@gmail.com>
2017-04-09 22:47:59 +0100

    Added login screen

--- FlappyClone/api/views.py
+++ FlappyClone/api/views.py
@@ -4,36 +4,7 @@ from django.utils import timezone
 
 from .. import models
 
-# NB: If these constants are updated, remember to update the JavaScript versions (in js/score.js)!!
-NUM_LEADERBOARD_ENTRIES = 10
-MAX_NAME_LENGTH = 16
-LEGAL_SYMBOLS = '-_'
-
-# NB: If updating these functions, ensure that the JavaScript functions are also updated (in js/score.js)!
-def isValidNameChar(c):
-    if (c >= 'a' and c <= 'z'):
-        return True
-    elif (c >= 'A' and c <= 'Z'):
-        return True
-    elif (c >= '1' and c <= '9'):
-        return True
-    elif (LEGAL_SYMBOLS.find(c) != -1):
-        return True
-    else:
-        return False
-
-# NB: If updating these functions, ensure that the JavaScript functions are also updated (in js/score.js)!
-def isValidName(name):
-    reason = ''
-    if (len(name) == 0):
-        return (False, 'name is an empty string')
-    elif (len(name) > MAX_NAME_LENGTH):
-        return (False, 'name is too long (' + len(name) + ' characters, max is ' + MAX_NAME_LENGTH + ')')
-    else:
-        for c in name:
-            if (not isValidNameChar(c)):
-                return (False, 'name contains an invalid character (' + c + ')')
-    return True
+from .lib import *
 
 def leaderboard(request):
     json = '[' + ','.join(x.toJSON() for x in models.LeaderboardEntry.objects.order_by('-score')[:10]) + ']'
