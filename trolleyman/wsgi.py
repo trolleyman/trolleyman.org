be08693a1fa1be6924b106d962de9ae02324f566
Callum Tolley <cgtrolley@gmail.com>
2017-04-12 03:01:27 +0000

    Remove unnecessary files from wsgi.py

--- trolleyman/wsgi.py
+++ trolleyman/wsgi.py
@@ -9,12 +9,6 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 
 import os, sys
 
-# Add the trolleyman project path into the sys.path
-sys.path.append('/trolleyman.org/trolleyman')
-
-# Add the virtualenv site-packages path to the sys.path
-sys.path.append('/trolleyman.org/venv/Lib/site-packages')
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trolleyman.settings")
 
 from django.core.wsgi import get_wsgi_application
