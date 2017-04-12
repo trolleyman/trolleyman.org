8fabef95b1c9d631785b1eeb984135af004febb5
Callum Tolley <cgtrolley@gmail.com>
2017-04-12 03:10:27 +0100

    Updated wsgi.py

--- trolleyman/wsgi.py
+++ trolleyman/wsgi.py
@@ -7,10 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
-from django.core.wsgi import get_wsgi_application
+# Add the trolleyman project path into the sys.path
+sys.path.append('/trolleyman.org/trolleyman')
+
+# Add the virtualenv site-packages path to the sys.path
+sys.path.append('/trolleyman.org/venv/Lib/site-packages')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trolleyman.settings")
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
