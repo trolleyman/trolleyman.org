717a219371b174c6e0adcb9ecb3c07d5a0c7297b
Callum Tolley <cgtrolley@gmail.com>
2017-06-01 16:04:19 +0100

    Added random background to homepage

--- homepage/views.py
+++ homepage/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.contrib.staticfiles.templatetags.staticfiles import static
+
+import random
 
 def index(request):
-    return render(request, 'homepage/index.html', {})
+    num_bg = 18
+    i = random.randrange(num_bg)+1
+    bg_url = static('homepage/images/bg/{:02}.jpg'.format(i))
+    
+    return render(request, 'homepage/index.html', {'bg_url': bg_url})
