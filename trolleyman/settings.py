87537929d80989be195f8e6e5e7c07d24fd9e643
Callum Tolley <cgtrolley@gmail.com>
2017-07-19 16:22:38 +0100

    Added django-pipeline

--- trolleyman/settings.py
+++ trolleyman/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,14 +27,20 @@ with open(os.path.join(BASE_DIR, 'SECRET_KEY'), 'r') as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 if not os.path.exists(os.path.join(BASE_DIR, 'DEBUG')):
     DEBUG = False
-    ALLOWED_HOSTS = ['.trolleyman.org', 'trolleyman.org']
+    ALLOWED_HOSTS = ['.trolleyman.org', 'trolleyman.org', 'localhost']
     CONN_MAX_AGE = None
     ADMINS = [('Callum Tolley', 'cgtrolley@gmail.com')]
-    log_path = os.path.join(BASE_DIR, 'log.txt') # '/var/log/apache2/django.log'
 else:
     DEBUG = True
     ALLOWED_HOSTS = []
-    log_path = os.path.join(BASE_DIR, 'log.txt')
+
+if sys.platform.startswith('linux'):
+    LOG_PATH = '/var/log/apache2/django/django.log'
+else:
+    LOGS_DIR = os.path.join(BASE_DIR, 'logs')
+    if not os.path.exists(LOGS_DIR):
+        os.mkdir(LOGS_DIR)
+    LOG_PATH = os.path.join(LOGS_DIR, 'django.log')
 
 INTERNAL_IPS = (
     '127.0.0.1',
@@ -53,6 +60,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pipeline', # django-pipeline
 ]
 
 MIDDLEWARE = [
@@ -137,35 +145,97 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+STATICFILES_STORAGE = 'pipeline.storage.PipelineStorage' # django-pipeline
+
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'pipeline.finders.PipelineFinder', # django-pipeline
 ]
+
+PIPELINE = {
+    'STYLESHEETS': {
+        'homepage-main': {
+            'source_filenames': (
+                'homepage/css/normalize.css',
+                'homepage/css/main.css',
+                'homepage/css/main-mod.css',
+            ),
+            'output_filename': 'homepage/css/main.min.css',
+            'extra_context': {
+                'media': 'screen,projection',
+            },
+        },
+    },
+    'JAVASCRIPT': {
+    },
+    'CSS_COMPRESSOR': 'pipeline.compressors.yuglify.YuglifyCompressor',
+    'JS_COMPRESSOR': 'pipeline.compressors.yuglify.YuglifyCompressor',
+    'DISABLE_WRAPPER': True,
+    'YUGLIFY_BINARY': 'C:/Users/Callum/AppData/Roaming/npm/yuglify.cmd',
+}
 
 LOGIN_URL = 'login'
 LOGOUT_REDIRECT_URL = 'login'
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': log_path,
+if DEBUG:
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'handlers': {
+            'default': {
+                'level': 'DEBUG',
+                'class': 'logging.handlers.RotatingFileHandler',
+                'filename': LOG_PATH,
+                'maxBytes': 1024*1024*5, # 5 MB
+                'backupCount': 5,
+                'formatter': 'standard',
+            },  
+            'console': {
+                'class': 'logging.StreamHandler',
+                'formatter': 'standard',
+            },
         },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
+        'formatters': {
+            'standard': {
+                'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+            },
         },
-        'FlappyClone': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
+        'loggers': {
+            'django': {
+                'handlers': ['default', 'console'],
+                'level': 'DEBUG',
+                'propagate': True,
+            },
         },
-    },
-}
-
+    }
+else:
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'handlers': {
+            'default': {
+                'level': 'DEBUG',
+                'class': 'logging.handlers.RotatingFileHandler',
+                'filename': LOG_PATH,
+                'maxBytes': 1024*1024*5, # 5 MB
+                'backupCount': 5,
+                'formatter': 'standard',
+            },  
+            'console': {
+                'class': 'logging.StreamHandler',
+            },
+        },
+        'formatters': {
+            'standard': {
+                'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+            },
+        },
+        'loggers': {
+            '': {
+                'handlers': ['default'],
+                'level': 'DEBUG',
+                'propagate': True,
+            },
+        },
+    }
