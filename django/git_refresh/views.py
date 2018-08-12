69e37e6b32e20883e57707bc467523bb6e5a6462
Callum Tolley <cgtrolley@gmail.com>
2018-08-12 20:12:25 +0000

    Fixed some bugs, added signature verification that doesn't work yet

--- django/git_refresh/views.py
+++ django/git_refresh/views.py
@@ -1,20 +1,68 @@
-from django.http import HttpResponse
+import os
+
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.views.decorators.csrf import csrf_exempt
 
 import json
+import hmac
 
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+with open(os.path.join(BASE_DIR, 'keys/GIT_REFRESH_SECRET'), 'r') as f:
+	hex = f.read().strip()
+	SECRET = bytearray.fromhex(hex)
 
 PORT=9401
 
+@csrf_exempt
 def push_hook(request):
 	# A push has been triggered
+	# Get signature
+	try:
+		sig = request.META['HTTP_X_HUB_SIGNATURE']
+	except KeyError:
+		return HttpResponseBadRequest('Required header X-Hub-Signature not found')
+
+	# Validate signature
+	if not is_signature_valid(sig, request.body):
+		ret = HttpResponse('Invalid signature')
+		ret.status_code = 401
+		return ret
+
+	# Decode JSON
 	body = request.body.decode(encoding='utf-8', errors='replace')
 	try:
 		js = json.loads(body)
 	except json.JSONDecodeError:
 		return HttpResponseBadRequest('Invalid JSON')
 
-	print(js)
+	# Get event name
+	print(request.META)
+	try:
+		evt_name = request.META['HTTP_X_GITHUB_EVENT']
+	except KeyError:
+		return HttpResponseBadRequest('Required header X-Github-Event not found')
+
+	# Get keys from json
+	try:
+		ref = js['ref']
+
+	except KeyError:
+		return HttpResponseBadRequest('Invalid JSON')
+
+	# Signal that there has been a push request
+	# TODO
 
 	# Return a default OK response
 	return HttpResponse('ok')
+
+def is_signature_valid(sig, body):
+	digest = hmac.new(SECRET, msg=body, digestmod='sha1').hexdigest()
+	calc_sig = 'sha1=%s' % digest
+	import logging
+	log = logging.getLogger(__name__)
+	log.warning('calc_sig: %r' % calc_sig)
+	log.warning('sig     : %r' % sig)
+	return hmac.compare_digest(sig, calc_sig)
 
--- django/trolleyman/settings.py
+++ django/trolleyman/settings.py
@@ -13,12 +13,9 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 import sys
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 with open(os.path.join(BASE_DIR, 'keys/SECRET_KEY'), 'r') as f:
