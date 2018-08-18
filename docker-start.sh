5d0626cff09fa6b99643dfb7272812cc32480097
Callum Tolley <cgtrolley@gmail.com>
2018-08-19 00:10:22 +0100

    Re-fix docker-start.sh

--- docker-start.sh
+++ docker-start.sh
@@ -6,21 +6,20 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export COM_PORT=9401
 
-cd $DIR
+cd $DIR/django
 
-cd django
 git pull || true # We don't want spurious network errors holding up the server
 
-cd ..
+cd $DIR
 
 docker build . -t server
 docker kill server || true
 docker rm server || true
 docker run -d\
-  -v logs:/django/logs \
-  -v django/database:/django/database \
-  -v logs:/caddy/logs \
-  -v .caddy:/caddy/.caddy \
+  -v $DIR/logs:/django/logs \
+  -v $DIR/django/database:/django/database \
+  -v $DIR/logs:/caddy/logs \
+  -v $DIR/.caddy:/caddy/.caddy \
   -p 80:80 -p 443:443 -p $COM_PORT:$COM_PORT \
   -e COM_PORT=$COM_PORT \
   --name server \
