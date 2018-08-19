2d23b4cd0c02f2165c2e5f83bd6d1fda6a82955e
Callum Tolley <cgtrolley@gmail.com>
2018-08-19 12:44:16 +0100

    Fixed logging

--- django/trolleyman/settings.py
+++ django/trolleyman/settings.py
@@ -194,7 +194,7 @@ LOGGING = {
         },
         'rotating_file': {
             'level': 'DEBUG',
-            'class': 'logging.FileHandler',
+            'class': 'logging.RotatingFileHandler',
             'filename': LOG_PATH,
             'maxBytes': 1024*1024*5,  # 5 MB
             'backupCount': 10,
