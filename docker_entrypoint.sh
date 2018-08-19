4654e68ace9446443b804f2bb94a242678a859bb
Callum Tolley <cgtrolley@gmail.com>
2018-08-19 17:14:03 +0100

    Fix docker_entrypoint.sh

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -12,7 +12,7 @@ caddy --conf Caddyfile --log logs/caddy.log 2>&1 > /dev/null &
 cd /opt/django
 
 # Migrate the database
-python manage.py migrate
+python3 manage.py migrate
 
 # Run django via gunicorn & wait for this to exit
 gunicorn -b localhost:${DJANGO_PORT} trolleyman.wsgi > logs/gunicorn.log 2>&1
