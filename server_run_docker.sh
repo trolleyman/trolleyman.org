2db20930e76c1b0e597f4d0fc190efd495341f8e
Callum Tolley <cgtrolley@gmail.com>
2019-12-27 12:48:37 +0000

    Add docker image pruning

--- server_run_docker.sh
+++ server_run_docker.sh
@@ -8,6 +8,10 @@ set -ex
 # Get dir of script location
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Prune images that are older than 2 months
+docker image prune --filter='until=1460h' -f
+
+# Build server docker image
 docker build "$DIR" -t server
 docker stop server || true
 docker rm server || true
