31ec9464ac69812c870965f33c80203b23426397
Callum Tolley <cgtrolley@gmail.com>
2020-01-06 22:27:44 +0000

    Fix typescript filter

--- django/trolleyman/filters.py
+++ django/trolleyman/filters.py
@@ -1,4 +1,6 @@
 from shutil import which
+import subprocess
+from tempfile import NamedTemporaryFile
 
 from django.core.exceptions import ImproperlyConfigured
 
@@ -6,13 +8,20 @@ from compressor.filters import CompilerFilter
 
 
 class TypescriptFilter(CompilerFilter):
+    command = "tsc --outFile {outfile} {infile}"
+    
     def __init__(self, *args, **kwargs):
-        super(TypescriptFilter, self).__init__(*args, **kwargs)
+        super(TypescriptFilter, self).__init__(, *args, **kwargs)
         if which('tsc') is None:
             raise ImproperlyConfigured("TypeScript compiler isn't installed (`tsc` is not on the path)")
 
     def input(self, **kwargs):
+        self.infile = NamedTemporaryFile(mode='wb', suffix='.ts')
+        self.infile.write(self.content.encode(encoding))
+        self.infile.flush()
+        self.options["infile"] = self.infile.name
 
+        return super(TypescriptFilter, self).input(**kwargs)
 
 """
     command = None
