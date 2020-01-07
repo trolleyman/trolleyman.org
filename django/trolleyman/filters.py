93d21c4a49456eb706cd822ba5bd014cbecdd548
Callum Tolley <cgtrolley@gmail.com>
2020-01-07 18:05:33 +0000

    Add inline source map when typescript compiling

--- django/trolleyman/filters.py
+++ django/trolleyman/filters.py
@@ -3,16 +3,19 @@ import subprocess
 from tempfile import NamedTemporaryFile
 
 from django.core.exceptions import ImproperlyConfigured
+from django.conf import settings
 
 from compressor.filters import CompilerFilter
 
 
 class TypescriptFilter(CompilerFilter):
-    command = "tsc --outFile {outfile} {infile}"
-    
     def __init__(self, *args, **kwargs):
+        self.command = "tsc --module amd --outFile {outfile} {infile}"
+        if settings.DEBUG:
+            self.command += ' --inlineSourceMap --inlineSources'
+            #self.command += ' -sourcemap'
+
         super(TypescriptFilter, self).__init__(*args, **kwargs)
-        print(self.__dict__)
         if which('tsc') is None:
             raise ImproperlyConfigured("TypeScript compiler isn't installed (`tsc` is not on the path)")
 
