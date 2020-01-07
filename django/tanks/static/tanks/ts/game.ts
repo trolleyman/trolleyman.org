9a5f9fd7369ef60e9defd7d50bcb9bcd6d536e85
Callum Tolley <cgtrolley@gmail.com>
2020-01-07 18:04:33 +0000

    Add tanks structure

--- django/tanks/static/tanks/ts/game.ts
+++ django/tanks/static/tanks/ts/game.ts
@@ -1,1 +1,34 @@
-console.log("TS loaded");
+
+export class Game {
+	canvas: HTMLCanvasElement;
+	context: CanvasRenderingContext2D;
+
+	constructor() {
+		this.canvas = <HTMLCanvasElement>document.getElementById('canvas');
+		this.context = this.canvas.getContext('2d');
+		window.requestAnimationFrame(() => this.loop());
+	}
+
+	loop() {
+		window.requestAnimationFrame(() => this.loop());
+		
+		// Update
+		this.update();
+		
+		// Draw
+		this.draw();
+	}
+	
+	update() {
+		
+	}
+	
+	draw() {
+		
+	}
+}
+
+new Game();
+
+
+
