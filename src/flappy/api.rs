81e10403037bda6190736d0f68dea23e206724fa
Callum Tolley <cgtrolley@gmail.com>
2020-01-21 01:02:18 +0000

    Fix leaderboard submitting

--- src/flappy/api.rs
+++ src/flappy/api.rs
@@ -1,9 +1,11 @@
 
 use serde::Serialize;
 use chrono::prelude::*;
+use rocket::request::LenientForm;
 use rocket_contrib::json::Json;
 use diesel::prelude::*;
 
+use crate::schema::flappy_leaderboard as leaderboard;
 use crate::DbConn;
 
 
@@ -12,8 +14,15 @@ struct LeaderboardEntry {
 	id: i32,
 	name: String,
 	score: i32,
-    #[serde(with = "serde_naive_datetime")]
+	#[serde(with = "serde_naive_datetime")]
 	timestamp: chrono::NaiveDateTime,
+}
+
+#[derive(Insertable, FromForm)]
+#[table_name="leaderboard"]
+struct NewLeaderboardEntry {
+	pub name: String,
+	pub score: i32,
 }
 
 mod serde_naive_datetime {
@@ -32,15 +41,20 @@ mod serde_naive_datetime {
 
 
 pub fn routes() -> Vec<rocket::Route> {
-	routes![leaderboard]
+	routes![leaderboard, submit]
 }
 
 #[get("/api/leaderboard")]
 fn leaderboard(conn: DbConn) -> Json<Vec<LeaderboardEntry>> {
-	use crate::schema::flappy_leaderboard as leaderboard;
 	Json(leaderboard::table
 		.order(leaderboard::score.desc())
 		.limit(10)
 		.load(&*conn)
 		.unwrap_or_else(|_| vec![]))
+}
+
+#[post("/api/submit", data = "<leaderboard_entry>")]
+fn submit(leaderboard_entry: LenientForm<NewLeaderboardEntry>, conn: DbConn) -> Result<String, String> {
+	leaderboard_entry.0.insert_into(leaderboard::table)
+		.execute(&*conn).map(|_| format!("Success")).map_err(|_| format!("Error inserting new entry"))
 }
