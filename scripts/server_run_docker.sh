e44c229d40b22881a5e11e3815830e0b4d34475d
Callum Tolley <cgtrolley@gmail.com>
2020-02-02 17:14:46 +0200

    Modify script, hope to fix issues

--- scripts/server_run_docker.sh
+++ scripts/server_run_docker.sh
@@ -13,27 +13,31 @@ cd "$DIR"
 docker image prune --filter='until=1460h' -f
 
 # Build server docker image
-docker build -t server "$DIR"
+docker build -t server .
 
 # Stop old server, and rebuild anew
 docker stop server || true
 docker rm server || true
 rm -f "$DIR/scripts/restart_flag/restart_flag" || true
 docker run --rm \
-  -d \
-  -v "$DIR/logs:/trolleyman.org/logs" \
-  -v "$DIR/database:/trolleyman.org/database" \
-  -v "$DIR/.caddy:/trolleyman.org/.caddy" \
-  -v "$DIR/scripts/restart_flag:/trolleyman.org/restart_flag" \
-  -p 80:80 -p 443:443 \
-  --name server \
-  server
+    -d \
+    -v "$DIR/logs:/trolleyman.org/logs" \
+    -v "$DIR/database:/trolleyman.org/database" \
+    -v "$DIR/.caddy:/trolleyman.org/.caddy" \
+    -v "$DIR/scripts/restart_flag:/trolleyman.org/restart_flag" \
+    -p 80:80 -p 443:443 \
+    --name server \
+    server
 
 # Wait for restart flag
 set +x
 echo "Started waiting..."
-while ! [[ -e "$DIR/scripts/restart_flag/restart_flag" ]]; do
-    inotifywait -e create -e modify -e delete -e close -e open -e move --timeout 10 "$DIR/scripts/restart_flag" >/dev/null 2>&1 || true
+while ! [[ -e scripts/restart_flag/restart_flag ]]; do
+    inotifywait -e create -e modify -e delete -e close -e open -e move --timeout 10 scripts/restart_flag >/dev/null 2>&1 || true
 done
-if ! [[ -e "$DIR/scripts/restart_flag/restart_flag" ]]; then echo "Restart flag detected"; else echo "no restart, but exiting while -- why??"; fi
+if ! [[ -e scripts/restart_flag/restart_flag ]]; then
+    echo "Restart flag detected"
+else
+    echo "No restart flag detected, but exiting while for some reason"
+fi
 echo "Done waiting."
