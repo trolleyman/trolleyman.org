5fe30236dfdff76b788fcdd5d0fa75babcf8fac3
Callum Tolley <cgtrolley@gmail.com>
2020-03-23 23:24:00 +0000

    Implement login and register pages

--- src/account.rs
+++ src/account.rs
@@ -1,28 +1,46 @@
 
 use rocket_contrib::templates::Template;
 
-pub fn routes() -> Vec<rocket::Route> { routes![signin_get, signin_post, signup_get, signup_post] }
+pub fn routes() -> Vec<rocket::Route> { routes![login_get, login_post, register_get, register_post] }
 
-#[get("/signin")]
-fn signin_get() -> Template {
-	Template::render("account/signin", json!({}))
+
+pub const USERNAME_REGEX: &'static str = "^\\w(\\w|[-_.])+$";
+pub const USERNAME_MIN_LENGTH: i32 = 3;
+pub const USERNAME_MAX_LENGTH: i32 = 20;
+pub const EMAIL_MAX_LENGTH: i32 = 30;
+pub const PASSWORD_MIN_LENGTH: i32 = 8;
+pub const PASSWORD_MAX_LENGTH: i32 = 32;
+
+
+fn default_context(patch: serde_json::Value) -> serde_json::Value {
+	let mut ctx = json!({
+		"username_regex": USERNAME_REGEX,
+		"username_min_length": USERNAME_MIN_LENGTH,
+		"username_max_length": USERNAME_MAX_LENGTH,
+		"email_max_length": EMAIL_MAX_LENGTH,
+		"password_min_length": PASSWORD_MIN_LENGTH,
+		"password_max_length": PASSWORD_MAX_LENGTH,
+	});
+	json_patch::merge(&mut ctx, &patch);
+	ctx
 }
 
-#[post("/signin")]
-fn signin_post() -> Template {
-	Template::render("account/signin", json!({
-		// TODO
-	}))
+#[get("/login")]
+fn login_get() -> Template {
+	Template::render("account/login", default_context(json!({})))
 }
 
-#[get("/signup")]
-fn signup_get() -> Template {
-	Template::render("account/signup", json!({}))
+#[post("/login")]
+fn login_post() -> Template {
+	Template::render("account/login", default_context(json!({})))
 }
 
-#[post("/signup")]
-fn signup_post() -> Template {
-	Template::render("account/signup", json!({
-		// TODO
-	}))
+#[get("/register")]
+fn register_get() -> Template {
+	Template::render("account/register", default_context(json!({})))
+}
+
+#[post("/register")]
+fn register_post() -> Template {
+	Template::render("account/register", default_context(json!({})))
 }
